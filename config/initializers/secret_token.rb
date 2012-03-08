6cb9c8a82d9621a3bc6a4283145626b8ddf0c7fb
Jason Gifford <jgifford78@gmail.com>
2012-03-08 12:50:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoutMasterTools::Application.config.secret_token = '1be3fc8c65b47c6d59d28542cd4012ced629038950829049e12cea0add410012b721e7077dc30925bbe993fdf7a1bfd61e381f10f55cde1c4cdfcb64fd39e75d'
